b15ad6ef17ad576f7a30e5caf96412d263666684
kairayary <kaira.abreu@gmail.com>
2022-08-31 19:04:39 -0300

    creacion de funcion adivinar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var juego = {
     palabra: "ALURA",
-    estado: 1,
+    estado: 7,
     acierto: ["A", "L"],
     errado: ["B", "J", "K", "C"]
 
@@ -15,12 +15,18 @@ function dibujo(juego) {
     //para cambiar la imagen a medida que transcurre el juego
     var $elem
     $elem = $html.cuerpo
-    $elem.src = "./img/0" + juego.estado + ".png"
+    var estado= juego.estado
+    if(estado==8){
+       estado = estado.previo
+    }
+    $elem.src = "./img/0" + estado + ".png"
 
     //creación de las letras adivinadas
     var palabra = juego.palabra
     var acierto = juego.acierto
     $elem = $html.acierto
+    //para eliminar elementos previos
+    $elem.innerHTML= ""
     for (let letra of palabra) {
         let $span = document.createElement("span")
         let $txt = document.createTextNode("")
@@ -34,6 +40,7 @@ function dibujo(juego) {
     //creación de letras erradas
     var errado = juego.errado
     $elem = $html.errado
+    $elem.innerHTML= ""
      for (let letra of errado){
         $span = document.createElement("span")
         $txt = document.createTextNode(letra)
@@ -44,4 +51,45 @@ function dibujo(juego) {
 
 }
 
+function adivinar(juego, letra) {
+    let estado = juego.estado
+    if (estado === 1 || estado === 8) {
+        return
+        
+    }
+    var acierto = juego.acierto
+    var errado = juego.errado
+    if (acierto.indexOf(letra)>= 0 || errado.indexOf(letra)>=0){
+        return
+        
+    }
+    var palabra = juego.palabra
+    if (palabra.indexOf(letra)>=0) {
+      let ganado = true
+        for(let l of palabra){
+          if(acierto.indexOf(l)< 0 && l!==letra){
+             ganado=false
+             juego.previo = juego.estado
+             break
+          }
+      } 
+      if(ganado){
+        juego.estado=8
+      }
+      acierto.push(letra)
+    }else{
+        juego.estado--
+        errado.push(letra)
+    }
+}
+
+window.onkeypress= function adivinarLetra(e){
+  var letra = e.key
+  letra = letra.toUpperCase()
+  if(/[^A-ZÑ]/.test(letra)){
+    return
+  }
+  adivinar(juego, letra)
+  dibujo(juego)
+}
 dibujo(juego)
